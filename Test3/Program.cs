a02c793d6b727b672313c5fbfade37a3a831f55f
Aleksandr <kozhan13@mail.ru>
2024-03-11 01:59:49 +0300

    Аттестация

---
+++ Test3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] Array = { 1, 2, 3, 4, 5 };
+int g = Array.Length - 1;
+
+
+void Main()
+{
+    recursion(g);
+    Console.ReadKey();
+}
+void recursion(int x)
+{
+    int n = 0;
+    int[] InvertArray = new int[5];
+    if (x >= 0)
+    {
+        InvertArray[n] = Array[x];
+        Console.Write(InvertArray[n] + " ");
+        n++;
+        x--;
+    }
+    else
+    {
+        return;
+    }
+    recursion(x);
+}
+Main();
